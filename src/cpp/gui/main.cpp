b840da7c566acb24676aca6b3fd9a142fd8ff94f
nepomok <nepomok@outlook.com>
2021-03-14 10:50:47 +0100

    Increment version number

--- src/cpp/gui/main.cpp
+++ src/cpp/gui/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationDisplayName("Qt-Calculator");
-    a.setApplicationVersion("v0.0.1");
+    a.setApplicationVersion("v0.1.0");
 
     MainWindow w;
     w.show();
