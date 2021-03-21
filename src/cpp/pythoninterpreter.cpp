2e7b0cdc62769f9b40bf0a9c59b76b9084d8ec90
xenotux <xenotux@outlook.com>
2021-03-21 18:34:19 +0100

    Update script error exception.

--- src/cpp/pythoninterpreter.cpp
+++ src/cpp/pythoninterpreter.cpp
@@ -25,8 +25,31 @@ long double PythonInterpreter::run(const std::string &src, const std::vector<lon
     PyObject *result = PyRun_String(src.c_str(), Py_file_input, globalDictionary, localDictionary);
 
     if (result == nullptr) {
+        /*
+        //The documentation does not give clear insight as to what PyErr_Fetch does so i will not risk a segfault because of bad documentation.
+        PyObject *pType, *pValue, *pTraceback;
+        PyErr_Fetch(&pType, &pValue, &pTraceback);
+
+        PyObject *pTypeStr = PyObject_Str(pType);
+        PyObject *pValueStr = PyObject_Str(pValue);
+        PyObject *pTracebackStr = PyObject_Str(pTraceback);
+
+        const char *pErrorType = PyUnicode_AsUTF8(pTypeStr);
+        const char *pErrorValue = PyUnicode_AsUTF8(pValueStr);
+        const char *pErrorTraceback = PyUnicode_AsUTF8(pTracebackStr);
+
+        std::string error = "Failed to run script, Error{ Type: ";
+        error += pErrorType;
+        error += ", Value: ";
+        error += pErrorValue;
+        error += ", Traceback: ";
+        error += pErrorTraceback;
+        error += " }";
+
+        throw std::runtime_error(error);
+        */
         PyErr_Print();
-        throw std::runtime_error("Failed to run script");
+        throw std::runtime_error("Failed to run script, See standard error for a detailed error description");
     } else {
         PyObject *output = PyDict_GetItemString(localDictionary, "outv");
         if (output != nullptr) {
