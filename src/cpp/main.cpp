eadb12e230bb7c34deea6374134e25414a494e5a
xenotux <xenotux@outlook.com>
2021-04-05 14:08:15 +0200

    Change organization name to upper case

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    a.setOrganizationName("xenotux");
+    a.setOrganizationName("Xenotux");
     a.setApplicationName("qcalc");
     a.setApplicationDisplayName("QCalculator");
     a.setApplicationVersion("v0.2.0");
