e4de6ae7a534e20e495088f30c053b99752a88a3
xenotux <xenotux@outlook.com>
2021-04-07 00:10:07 +0200

    Update comment

--- src/cpp/calc/arithmetictype.hpp
+++ src/cpp/calc/arithmetictype.hpp
@@ -6,10 +6,13 @@
  *
  * For now we will use double to match the cpython float implementation.
  *
- * To achieve perfect precision a arbitrary precision number format has to be implemented.
+ * To achieve perfect precision a arbitrary precision number format has to be used.
+ *
+ * Exprtk supports custom types as demonstrated at:
+ * https://github.com/ArashPartow/exprtk-custom-types
  *
  * This could then be exposed to python with a custom type,
- * and would also have to be integrated with exprtk.
+ * and would also have to be integrated with exprtk as outlined in the above examples.
 */
 typedef double ArithmeticType;
 
