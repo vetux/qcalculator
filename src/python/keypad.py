b7540894d742908433228f3694b267ce899331f9
xenotux <xenotux@outlook.com>
2021-04-13 11:26:12 +0200

    Add license notices to source

--- src/python/keypad.py
+++ src/python/keypad.py
@@ -1,3 +1,19 @@
+# QCalc - Extensible programming calculator
+# Copyright (C) 2021  Julian Zampiccoli
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import qcalc.gui as gui
 from PySide2 import QtCore, QtWidgets
 
@@ -166,7 +182,8 @@ def load():
     global widget
     handler = KeyPadHandler()
     widget = KeyPadWidget(gui.wnd)
-    QtCore.QObject.connect(widget, QtCore.SIGNAL("signal_key_pressed(int)"), handler, QtCore.SLOT("slot_key_press(int)"))
+    QtCore.QObject.connect(widget, QtCore.SIGNAL("signal_key_pressed(int)"), handler,
+                           QtCore.SLOT("slot_key_press(int)"))
     gui.root.layout().addWidget(widget)
 
 
